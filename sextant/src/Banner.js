9e38ad3d89a6437f42c92dbffa4faa03f34622a9
saroz savar <sarojsabar12345@gmail.com>
2022-09-12 00:45:28 +0530

    heading added and create an component

---
+++ sextant/src/Banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <>
+      <section className="banner">
+        <div className="container">
+          <h1 className="banner_heading">
+            Cisco online training program
+          </h1>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Banner;
