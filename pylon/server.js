65d689e270473f42f2806e2b54ae7e3ddcd494b6
saroz savar <sarojsabar12345@gmail.com>
2022-09-12 09:22:23 +0530

    server update

--- pylon/server.js
+++ pylon/server.js
@@ -1,14 +1,26 @@
-const webSocketServer = require('websocket').server;
-const http = require('http');
+// const webSocketServer = require('websocket').server;
+// const http = require('http');
 
-const server = http.createServer();
-server.listen(55455);
-const wsServer = new webSocketServer({ httpServer: server });
+// const server = http.createServer();
+// server.listen(55455);
+// const wsServer = new webSocketServer({ httpServer: server });
 
-wsServer.on('request', function (request) {
-    console.log('establishing a new connection with client');
-    var connection = request.accept(null, request.origin);
-    setInterval(() => {
-        connection.sendUTF(new Date().getTime())
-    }, 100);
+// wsServer.on('request', function (request) {
+//     console.log('establishing a new connection with client');
+//     var connection = request.accept(null, request.origin);
+//     setInterval(() => {
+//         connection.sendUTF(new Date().getTime())
+//     }, 100);
+// });
+
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
 });
